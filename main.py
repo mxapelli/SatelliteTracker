87fc5b2eb40e8dd6a008bc4dde4646d2bda6f64a
Marc Xapelli <marc.xapelli@estudiantat.upc.edu>
2022-08-28 02:33:35 +0200

    some functions added

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ def index():
     print(satellites[0][0]['OBJECT_NAME']+str(satellites[0][0]['NORAD_CAT_ID']))
     return render_template('index.html',datos=jsonSat)
 
-
+pi=math.pi
 incTime=30
 JD=0.0
 @app.route('/<int:catnr>')
@@ -139,7 +139,6 @@ def satellite(catnr):
     hour=epochT[1][0]
     incl=datosSat[0]['INCLINATION']
     omega=datosSat[0]['RA_OF_ASC_NODE']
-    global ecc
     ecc=datosSat[0]['ECCENTRICITY']
     w=datosSat[0]['ARG_OF_PERICENTER']
     M=datosSat[0]['MEAN_ANOMALY']
@@ -177,31 +176,11 @@ def satellite(catnr):
     s=x.tm_yday
     s=s+x.tm_hour/24+x.tm_min/(24*60)+x.tm_sec/(24*60*60)
     ToA=s
-    unixepoch = 2440587.5                #JD at unix epoch: 0h (UTC) 1/1/1970
-    unixseconds=time.time() 
-    global JD           
-    JD = unixepoch + unixseconds / (86400);         #Julian Date now in UTC
-
-    #Compute current time in secs. from ToA
-    MJ2022 = 59580#MJD on 1/1/2022 at 00:00 UTC (see http://leapsecond.com/java/cal.htm)
-    J2022 = MJ2022 + 2400000.5 #JD on 1/1/2022 at 00:00 UTC
-    JToA = J2022 + ToA - 1#ToA in JD 
-    esec = 86400 * (JD - JToA) #temps transcorregut des de'l ToA en segons
-
-    #Find GAST in degrees at ToA
-    J2000 = 2451545.0                                #epoch is 1/1/2000 at 12:00 UTC
-    midnight = round(JToA) - 0.5                       #midnight of JToA
-    days_since_midnight = JToA - midnight
-    hours_since_midnight = days_since_midnight * 24
-    days_since_epoch = JToA - J2000
-    centuries_since_epoch = days_since_epoch / 36525
-    whole_days_since_epoch = midnight - J2000
-    GAST = 6.697374558 + 0.06570982441908 * whole_days_since_epoch + 1.00273790935 * hours_since_midnight + 0.000026 * centuries_since_epoch**2 #GAST in hours from ?
-    GASTh = GAST%24       #GAST in hours at ToA
-    GASTdegToA = 15 * 1.0027855 * GASTh     #GAST in degrees at ToA (approx. 361º/24h)
-
+    result=esecGAST(ToA)
+    esec=result[0]
+    GASTdegToA=result[1]
     
-    #Process satellite kep. elements							#Earth Mass
+    #Process satellite kep. elements
     T = 86400 / n                    #orbit period (secs.)
     n = 2 * pi / T                          #mean movement [rad/s]
     a = (Ge * Me / n**2)**(1/3)                   #semi-major axis [m]
@@ -213,87 +192,19 @@ def satellite(catnr):
     w =  w * pi / 180                   #argument of perigee [rad]
     Mo =  M* pi / 180                   #Mean anomaly at ToA [rad]
 
-    #Visibility User
+    #Visibility Area of User
     visCoord=visibilidadObs(a,latUser,longUser,altUser);
     Xvis=visCoord[0]
     Yvis=visCoord[1]
 
-    #Plot groundtrack for NT periods
-    t = esec
-    NT = 1
-    j=0
-    latmap=[]
-    longmap=[]
-    altmap=[]
-
-    R=[]
-    while t < esec + T * NT:
-
-        #ECEF coordinates
-        #Kepler2ECEF ##[x, y, z] = Kepler2ECEF(a, io, e, Oo, dO, w, Mo, n, t)
-        M = Mo + n * t #Mean anomaly now [rad]
-        #Solve Keppler equation for eccentric anomally iteratively
-        # Loop is exited when we reach the desired tolerance
-        tol = 10**(-8) #Converging tolerance
-        E = M
-        while True:
-            Eo = E
-            E = M + ecc * math.sin(Eo)
-            if abs(Eo - E) < tol: 
-                break
-
-        #True anomaly
-        cosv = (math.cos(E) - ecc) / (1 - ecc * math.cos(E))
-        sinv = math.sqrt(1 - ecc**2) * math.sin(E) / (1 - ecc * math.cos(E))
-        v = math.atan2(sinv, cosv)
-
-        #Cartesian coordinates within the orbital plane
-        u = v + w
-        r = a * (1 - ecc * math.cos(E))
-        O = Oo - 7.2921151467e-5 * t + dO * t #corrected right ascension (7.2921151467e-5 = rate of Earth rotation [rad/s])
-        xp = r * math.cos(u)
-        yp = r * math.sin(u)
-        
-        #ECEF coordinates [m]
-        x = xp * math.cos(O) - yp * math.cos(io) * math.sin(O)
-        y = xp * math.sin(O) + yp * math.cos(io) * math.cos(O)
-        z = yp * math.sin(io)
-
-        xmap.append(x)
-        ymap.append(y)
-        zmap.append(z)
-        R.append(math.sqrt(x**2+y**2+z**2))
-
-        #LLA coordinates [lat, lon, alt] = ECEF2LLA(x, y, z)
-        #Convert from cartesian (x,y,z) to LLA coordinates in datum WGS84
-        A=6378137 
-        E2=0.00669437999014  #datos elipsoide WGS84
-        B=A*math.sqrt(1-E2) 
-        EP=A*math.sqrt(E2)/B
-        
-        r=math.sqrt(x**2+y**2)
-        EE2=A**2-B**2
-        F=54*B**2*z**2
-        G=r**2+(1-E2)*z**2-E2*EE2
-        C=E2**2*(F*r**2)/G**3
-        S=(1+C+math.sqrt(C**2+2*C))**(1/3)
-        P=F/(3*G**2*(S+1/S+1)**2)
-        Q=math.sqrt(1+2*E2**2*P)
-        ro=-E2*P*r/(1+Q)+math.sqrt(0.5*A**2*(1+1/Q)-(1-E2)*P*(z**2)/Q/(1+Q)-0.5*P*r**2)
-        U=math.sqrt(z**2+(r-ro*E2)**2)
-        V=math.sqrt((1-E2)*z**2+(r-ro*E2)**2)
-        Zo=(B**2*z)/(A*V)
-        
-        ALT=U*(1-B**2/(A*V))
-        LAT=math.atan2((z+EP**2*Zo),r)
-        LONG=math.atan2(y,x)
-        latmap.append(LAT*180/pi)
-        longmap.append(LONG*180/pi)
-        altmap.append(ALT)
-        
-        #increase time
-        t = t + incTime
-        j=j+1
+    #Groundtrack for NT periods
+    coordinates=computeCoordinates(esec,T,n,a,io,ecc,Oo,dO,w,Mo)
+    xmap=coordinates[0]
+    ymap=coordinates[1]
+    zmap=coordinates[2]
+    latmap=coordinates[3]
+    longmap=coordinates[4]
+    altmap=coordinates[5]
 
     #Check Visibility of satellite
     elev=[]
@@ -310,7 +221,6 @@ def satellite(catnr):
         vis=1
     else:
         vis=0
-
 
     #Map values
     worldLat=[] 
@@ -352,13 +262,67 @@ def doppler(catnr):
         altUser= 4
 
     sat=mongo_db.satellites.find_one({"noradID": catnr})
+    if not (sat):
+        return render_template('error.html')
     name=sat["name"]
     freq=sat["freq"]
-    xmap=sat["xECEF"]
-    ymap=sat["yECEF"]
-    zmap=sat["zECEF"]
-    a=sat["a"]
-    ecc=sat["ecc"]
+
+    datosObtenidos=requests.get('https://celestrak.com/NORAD/elements/gp.php?CATNR='+str(catnr)+'&FORMAT=JSON')
+    check=jsonCheck(datosObtenidos)
+    if check=="error":
+        print("Error satellite does not exist")
+        return render_template('error.html')
+    datosSat= datosObtenidos.json()
+    print("The user has selected satellite: ",datosSat[0]['OBJECT_NAME'])
+    epoch=datosSat[0]['EPOCH']
+    epochT=str(epoch).split("T")
+    epochT[1]=epochT[1].split(".")
+    hour=epochT[1][0]
+    incl=datosSat[0]['INCLINATION']
+    omega=datosSat[0]['RA_OF_ASC_NODE']
+    ecc=datosSat[0]['ECCENTRICITY']
+    w=datosSat[0]['ARG_OF_PERICENTER']
+    M=datosSat[0]['MEAN_ANOMALY']
+    n=datosSat[0]['MEAN_MOTION']
+
+    if sat is not None:
+        name=sat["name"]
+        if sat["epoch"]!=epoch:
+            id = { "noradID": catnr }
+            newvalues = { "$set": { "epoch": epoch,"incl": incl,"ecc": ecc,"omega": omega,"w": w,"M":M,"n":n} }
+            mongo_db.satellites.update_one(id,newvalues)
+            print(name," has been updated")
+
+
+    #TIME TO TOA
+    date=epochT[0]
+    x=time.strptime(date+" "+ hour, "%Y-%m-%d %H:%M:%S") 
+    s=x.tm_yday
+    s=s+x.tm_hour/24+x.tm_min/(24*60)+x.tm_sec/(24*60*60)
+    ToA=s
+    result=esecGAST(ToA)
+    esec=result[0]
+    GASTdegToA=result[1]
+    
+    #Process satellite kep. elements
+    T = 86400 / n                    #orbit period (secs.)
+    n = 2 * pi / T                          #mean movement [rad/s]
+    a = (Ge * Me / n**2)**(1/3)                   #semi-major axis [m]
+    io =  incl * pi / 180                   #orbit inclination [rad]
+    ecc =  ecc                     #orbit eccentricity
+    #GAST at a given epoch is the RA of the Greenwich meridian at that epoch
+    Oo = (omega - GASTdegToA) * pi / 180      #Longitude of AN at ToA [rad] Aqui hay que poner GASTdegToA
+    dO = 0                                  #Rate of Right ascension
+    w =  w * pi / 180                   #argument of perigee [rad]
+    Mo =  M* pi / 180                   #Mean anomaly at ToA [rad]
+
+    #Groundtrack for NT periods
+    coordinates=computeCoordinates(esec,T,n,a,io,ecc,Oo,dO,w,Mo)
+    xmap=coordinates[0]
+    ymap=coordinates[1]
+    zmap=coordinates[2]
+
+
     print("Frequency of satellite ",freq)
     r=(1-ecc)*a
     v=math.sqrt(Ge*Me*((2/r)-(1/a)))
@@ -406,7 +370,6 @@ def doppler(catnr):
     fD=[]
     fDReal=[]
     for n in range(len(ECI)):
-        time=n*incTime;
 
         # Velocidad Observador en Funcion de la Latitud
         Gast=GastV[n]
@@ -543,7 +506,7 @@ def doppler(catnr):
     img.seek(0)
     plot_url = base64.b64encode(img.getvalue()).decode('utf8')
 
-
+    #Doppler new graph
     img2 = BytesIO()
         
     fig,ax = plt.subplots(facecolor='#383A3F')
@@ -577,7 +540,9 @@ def GAST(esec):
     #Computes GAST [deg] at current time + esec [sec]
     #esec = ToA time in secs. from/to now (esec < 0 means ToA is in the past)
     #Find Julian Date now
-    global JD
+    unixepoch = 2440587.5                #JD at unix epoch: 0h (UTC) 1/1/1970
+    unixseconds=time.time()        
+    JD = unixepoch + unixseconds / (86400); 
     JToA = JD + esec / 86400;                             #Julian Date at ToA
 
     #Greenwich Mean Sidereal Time (GMST) is the hour angle of the average position of the vernal equinox,
@@ -597,6 +562,33 @@ def GAST(esec):
     GASTh = GAST % 24;                #GAST in hours at ToA
     GASTdeg = 15 * 1.0027855 * GASTh;      #GAST in degrees at ToA (approx. 361º/24h)
     return GASTdeg
+
+def esecGAST(ToA):
+    unixepoch = 2440587.5                #JD at unix epoch: 0h (UTC) 1/1/1970
+    unixseconds=time.time()        
+    JD = unixepoch + unixseconds / (86400);         #Julian Date now in UTC
+
+    #Compute current time in secs. from ToA
+    MJ2022 = 59580#MJD on 1/1/2022 at 00:00 UTC (see http://leapsecond.com/java/cal.htm)
+    J2022 = MJ2022 + 2400000.5 #JD on 1/1/2022 at 00:00 UTC
+    JToA = J2022 + ToA - 1#ToA in JD 
+    esec = 86400 * (JD - JToA) #temps transcorregut des de'l ToA en segons
+
+    #Find GAST in degrees at ToA
+    J2000 = 2451545.0                                #epoch is 1/1/2000 at 12:00 UTC
+    midnight = round(JToA) - 0.5                       #midnight of JToA
+    days_since_midnight = JToA - midnight
+    hours_since_midnight = days_since_midnight * 24
+    days_since_epoch = JToA - J2000
+    centuries_since_epoch = days_since_epoch / 36525
+    whole_days_since_epoch = midnight - J2000
+    GAST = 6.697374558 + 0.06570982441908 * whole_days_since_epoch + 1.00273790935 * hours_since_midnight + 0.000026 * centuries_since_epoch**2 #GAST in hours from ?
+    GASTh = GAST%24       #GAST in hours at ToA
+    GASTdegToA = 15 * 1.0027855 * GASTh     #GAST in degrees at ToA (approx. 361º/24h)
+    esecGAST=[]
+    esecGAST.append(esec)
+    esecGAST.append(GASTdegToA)
+    return esecGAST
 
 def ECEF2ECI(X, Y, Z, B):
     ECI=[]
@@ -639,6 +631,94 @@ def ECEF2NED(ECEF,phi,lamda):
 
     NED=numpy.matmul(invM,ECEFt)
     return NED
+
+def computeCoordinates(esec,T,n,a,io,ecc,Oo,dO,w,Mo):
+    t = esec
+    NT = 1
+    j=0
+    xmap=[]
+    ymap=[]
+    zmap=[]
+    latmap=[]
+    longmap=[]
+    altmap=[]
+    R=[]
+    while t < esec + T * NT:
+
+        #ECEF coordinates
+        #Kepler2ECEF ##[x, y, z] = Kepler2ECEF(a, io, e, Oo, dO, w, Mo, n, t)
+        M = Mo + n * t #Mean anomaly now [rad]
+        #Solve Keppler equation for eccentric anomally iteratively
+        # Loop is exited when we reach the desired tolerance
+        tol = 10**(-8) #Converging tolerance
+        E = M
+        while True:
+            Eo = E
+            E = M + ecc * math.sin(Eo)
+            if abs(Eo - E) < tol: 
+                break
+
+        #True anomaly
+        cosv = (math.cos(E) - ecc) / (1 - ecc * math.cos(E))
+        sinv = math.sqrt(1 - ecc**2) * math.sin(E) / (1 - ecc * math.cos(E))
+        v = math.atan2(sinv, cosv)
+
+        #Cartesian coordinates within the orbital plane
+        u = v + w
+        r = a * (1 - ecc * math.cos(E))
+        O = Oo - 7.2921151467e-5 * t + dO * t #corrected right ascension (7.2921151467e-5 = rate of Earth rotation [rad/s])
+        xp = r * math.cos(u)
+        yp = r * math.sin(u)
+        
+        #ECEF coordinates [m]
+        x = xp * math.cos(O) - yp * math.cos(io) * math.sin(O)
+        y = xp * math.sin(O) + yp * math.cos(io) * math.cos(O)
+        z = yp * math.sin(io)
+
+        xmap.append(x)
+        ymap.append(y)
+        zmap.append(z)
+        R.append(math.sqrt(x**2+y**2+z**2))
+
+        #LLA coordinates [lat, lon, alt] = ECEF2LLA(x, y, z)
+        #Convert from cartesian (x,y,z) to LLA coordinates in datum WGS84
+        A=6378137 
+        E2=0.00669437999014  #datos elipsoide WGS84
+        B=A*math.sqrt(1-E2) 
+        EP=A*math.sqrt(E2)/B
+        
+        r=math.sqrt(x**2+y**2)
+        EE2=A**2-B**2
+        F=54*B**2*z**2
+        G=r**2+(1-E2)*z**2-E2*EE2
+        C=E2**2*(F*r**2)/G**3
+        S=(1+C+math.sqrt(C**2+2*C))**(1/3)
+        P=F/(3*G**2*(S+1/S+1)**2)
+        Q=math.sqrt(1+2*E2**2*P)
+        ro=-E2*P*r/(1+Q)+math.sqrt(0.5*A**2*(1+1/Q)-(1-E2)*P*(z**2)/Q/(1+Q)-0.5*P*r**2)
+        U=math.sqrt(z**2+(r-ro*E2)**2)
+        V=math.sqrt((1-E2)*z**2+(r-ro*E2)**2)
+        Zo=(B**2*z)/(A*V)
+        
+        ALT=U*(1-B**2/(A*V))
+        LAT=math.atan2((z+EP**2*Zo),r)
+        LONG=math.atan2(y,x)
+        latmap.append(LAT*180/pi)
+        longmap.append(LONG*180/pi)
+        altmap.append(ALT)
+        
+        #increase time
+        t = t + incTime
+        j=j+1
+    coordinates=[]
+    coordinates.append(xmap)
+    coordinates.append(ymap)
+    coordinates.append(zmap)
+    coordinates.append(latmap)
+    coordinates.append(longmap)
+    coordinates.append(altmap)
+    return coordinates
+
 
 #define function to calculate cross product 
 def cross(a, b):
